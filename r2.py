60b9328059e62b01311cea657b6267a1a55016bb
gwitfeng <gwitfeng@gmail.com>
2018-09-25 21:56:09 +0800

    add r2.py for line chating story

--- r2.py
+++ r2.py
@@ -1,30 +1,63 @@
-# chating convert
 
 name = ['Stanley', 'Mindy']
 lines = []
 def read_file(filename) :
     with open (filename, 'r', encoding = 'utf-8-sig') as f :
         for line in f :
-            lines.append(line.strip() )
+            lines.append(line.strip())
         return lines
 
-
 def convert(lines) :
-    
-    person = None
+    new = []
+    name = None
+    stanley_word_count = 0
+    stanley_stic_count = 0
+    stanley_photo_count = 0
+    mindy_word_count = 0
+    mindy_stic_count = 0
+    mindy_photo_count = 0
+
     for line in lines :
-        print(line)  
+        s = line.split(' ')
+        print(s)
+        time = s[0]
+        name = s[1]
+        #print(s)
+       
+        if name == 'Stanley' :
+            if s[2] == '[貼圖]':
+                stanley_stic_count += 1
+            elif s[2] == '[照片]':
+                stanley_photo_count += 1
+            else :
+                for m in s[2:]:
+                    stanley_word_count += len(m)
+        elif name == 'Mindy' :
+            if s[2] == '[貼圖]':
+                mindy_stic_count += 1
+            elif s[2] == '[照片]':
+                mindy_photo_count += 1
+            else :
+                for m in s[2:]:
+                    mindy_word_count += len(m)
+    
+    print('Stanley 共說了 ', stanley_word_count, '個字')
+    print('Stanley 共傳了 ', stanley_stic_count, '個貼圖')
+    print('Stanley 共傳了 ', stanley_photo_count, '張照片')
+    print('Mindy 共說了 ', mindy_word_count, '個字')
+    print('Mindy 共傳了 ', mindy_stic_count, '個貼圖')
+    print('Mindy 共傳了 ', mindy_photo_count, '張照片')
             
 
-def write_file(filename, talks):
+
+def write_file(filename, lines):
     with open(filename, 'w') as f :
-        for line in lines :
+        for line in liness :
             f.write(line + '\n')
 
-
 def main() :  
-    lines = read_file('[LINE]Stanley.txt')
-    talklines = convert(lines)
+    lines = read_file('line_stanley.txt')
+    lines = convert(lines)
     #write_file('output.txt', lines)
 
-main()    
+main()     
